1e5c976920f5637d022bc5f169a86433b6d7ea3c
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-15 22:18:35 +0200

    test

--- css/css_config.php
+++ css/css_config.php
@@ -1,13 +1,9 @@
+
 <html>
-	<script src="/css/jQuery.js"></script>
-	<link rel="stylesheet" href="/css/materialize/css/materialize.min.css">
-	<script src="/css/materialize/js/materialize.min.js"></script>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-	<link rel="stylesheet" href="/css/style.css">
-	<link rel="icon" type="image/png" href="https://tournoisinterpolytech.files.wordpress.com/2014/10/cropped-1252.jpeg" />
+	<link rel="stylesheet" href="materialize/css/materialize.min.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="materialize.min.js"></script>
+	<script src="jQuery.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </html>
-<script>
-$(document).ready(function() {
-    $('select').material_select();
-  });
-</script>
