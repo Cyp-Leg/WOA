f05e50dff08a3b87a5789047aceaf40f3f1b7ae8
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-15 21:41:13 +0200

    test

--- css/css_config.php
+++ css/css_config.php
@@ -4,6 +4,6 @@
 	<link rel="stylesheet" href="style.css">
 	<script src="materialize.min.js"></script>
 	<script src="jQuery.js"></script>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 </html>
--- view/accueil.php
+++ view/accueil.php
@@ -1,42 +1,14 @@
 <!doctype html>
 <html lang="fr">
 	<head>
-		<title>Accueil Test Riasec</title>
-		<meta name="Content-Type" content="UTF-8">
-		<meta name="Content-Language" content="fr">
-		<meta name="Description" content="A MODIFIER">
-		<meta name="Keywords" content="A MODIFIER">
-		<meta name="Subject" content="A MODIFIER ">
-		<meta name="Copyright" content="A MODIFIER>
-		<meta name="Author" content="PolytechMontpellier">
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-		 <?php include ("/opt/lampp/htdocs/WOA/css/css_config.php") ?>
+		<title>Accueil</title>
+
+		 <?php// include ("/opt/lampp/htdocs/WOA/css/css_config.php") ?>
 	</head>
 	<body>
 		<?php require ("header.php");?>
 		<div>
 			<h4 class="center-align">Bienvenue sur XXX</h4>
-		</div>
-		<br>
-		<div class="container">
-			<div class="row rowSpe">
-				<a href="Test.php" class="col s12 m6 center-align button2" ><i class="amazing material-icons">play_circle_outline</i><h5>Faire le test</h5></a>
-				<a href="Test.php" class="col s12 m6 center-align button2" ><i class="amazing material-icons">replay</i><h5>Reprendre le test</h5></a>
-				<a href="Resultat.php" class="col s12 m6 center-align button2"><i class="amazing material-icons">class</i><h5>Revoir ses résultats</h5></a>
-				<a href="Administrateur.php" class="col s12 m6 center-align button2"><i class="amazing material-icons">supervisor_account</i><h5>Administrateur</h5></a>
-			  <a href="Profil.php" class="col s12 m6 center-align button2"><i class="amazing material-icons">perm_identity</i><h5>Profil</h5></a>
-				<a href="Connexion.php" class="col s12 m6 center-align button2"><i class="amazing material-icons">input</i><h5>Connexion</h5></a>
-			</div>
-			<br>
-			<div class="row z-depth-4 blue-grey lighten-5">
-				<div class="input-field col s12">
-					<h4>Analyse du questionnaire</h4>
-					<p>Ce questionnaire s’appuie sur une typologie définie par John HOLLAND, psychologue de formation. Elle distingue 6 types d’intérêts professionnels, chacun renvoyant aux environnements professionnels et aux métiers qui correspondent au mieux à chaque personnalité.<br>
-					Trouver un métier qui convient à qui l’on est vraiment est un gage de stabilité, de satisfaction et de réussite professionnelle. L’idée est, qu’en connaissant son profil typologique, un peut faire un pronostic sur le type d’environnement professionnel qui nous convient.<br>
-					Aucun d’entre nous ne peut se définir exclusivement au travers d’un seul type mais l’on peut toutefois déterminer celui qui est dominant (score le plus élevé).</p>
-				</div>
-			</div>
 		</div>
 		<?php require ("footer.php");?>
 	</body>
