551f51f2bd8031007df8daf4adfbdd554373b549
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-15 21:52:26 +0200

    adding css

--- css/css_config.php
+++ css/css_config.php
@@ -1,9 +1,13 @@
-
 <html>
-	<link rel="stylesheet" href="materialize.css">
-	<link rel="stylesheet" href="style.css">
-	<script src="materialize.min.js"></script>
-	<script src="jQuery.js"></script>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+	<script src="/css/jQuery.js"></script>
+	<link rel="stylesheet" href="/css/materialize/css/materialize.min.css">
+	<script src="/css/materialize/js/materialize.min.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="icon" type="image/png" href="https://tournoisinterpolytech.files.wordpress.com/2014/10/cropped-1252.jpeg" />
 </html>
+<script>
+$(document).ready(function() {
+    $('select').material_select();
+  });
+</script>
