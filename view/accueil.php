fc6a79f403fa0b604e024789c069d51e9b52b759
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-16 19:08:10 +0200

    adding favicon

--- view/accueil.php
+++ view/accueil.php
@@ -11,6 +11,7 @@
 		<meta name="Author" content="CyprienLegrand">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+		<link rel="icon" href="medias/favicon.ico" />
 		 <?php include ("css/css_config.php") ?>
 	</head>
 	<body>
