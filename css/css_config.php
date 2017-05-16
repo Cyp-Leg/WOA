249648a9318bf8625fde3399c905bd40c7187106
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-16 20:58:20 +0200

    logo

--- css/css_config.php
+++ css/css_config.php
@@ -1,6 +1,6 @@
 
 <html>
-	<link rel="stylesheet" href="/css/materialize/css/materialize.min.css">
+	<link rel="stylesheet" href="css/materialize/css/materialize.min.css">
 	<link rel="stylesheet" href="style.css">
 	<script src="materialize.min.js"></script>
 	<script src="jQuery.js"></script>
