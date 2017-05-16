e99c296283ecee06f0817a032336ec3819ac6ec9
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-16 21:02:32 +0200

    logo

--- css/css_config.php
+++ css/css_config.php
@@ -2,8 +2,8 @@
 <html>
 	<link rel="stylesheet" href="css/materialize/css/materialize.min.css">
 	<link rel="stylesheet" href="style.css">
-	<script src="materialize.min.js"></script>
-	<script src="jQuery.js"></script>
+	<script src="css/materialize/js/materialize.min.js"></script>
+	<script src="css/jQuery.js"></script>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="icon" type="image/png" href="medias/logo.ico" />
 
