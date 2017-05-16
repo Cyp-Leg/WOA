f24998a10c83e99275deba81b880ccb5c09d7ad6
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-16 22:11:27 +0200

    m

--- model/Pdo.php
+++ model/Pdo.php
@@ -1,14 +1,16 @@
 <?php
 function connexion()
 {
-	try
-	{
-		$heroku = new PDO('mysql:host=ec2-107-20-141-145.compute-1.amazonaws.com;dbname=dab5use60t2lcj;charset=utf8','rdsvzxqbycrkku','cc7fb0ce567ba1edcc6c6a87be84b98d0058b4e7efac1ebe0cd11a73eccf9640');
-	}
-	catch (Exception $e)
-	{
-			die('Erreur : ' . $e->getMessage());
-	}
-	return($heroku);
+	<?php
+$dsn = "pgsql:"
+    . "host=ec2-184-73-194-179.compute-1.amazonaws.com;"
+    . "dbname=ul28zxpr39no1rr;"
+    . "user=dj1wcxb3x9fy3x5;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=p28xwd9pjcrzyzp6mf74m99cze";
+
+$db = new PDO($dsn);
+return($db);
 }
 ?>
