57f74a08d942fecaf32ef0a9a8093a0e39dc7694
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-16 21:17:06 +0200

    logo

---
+++ controller/Controller_Inscription.php
@@ -0,0 +1,21 @@
+<?php
+require_once ("model/User.php");
+
+$firstname = htmlspecialchars($_POST['firstname']);
+$lastName = htmlspecialchars($_POST['lastname']);
+$nick = htmlspecialchars($_POST['nick']);
+$mail = htmlspecialchars($_POST['email']);
+$gender = htmlspecialchars($_POST['gender']);
+
+
+if(empty($firstname) || empty($lastname) || empty($nick) || empty($mail) || empty($gender)){
+  ?><script>alert("Veuillez remplir tous les champs!")</script>
+  <?php head("Location: ../Inscription.php");
+}
+else {
+  Users::Add_User($firstname,$lastname,$nick,$mail,$gender);
+  header("Location: ../Accueil.php");
+}
+
+require_once('view/inscription.php');
+?>
