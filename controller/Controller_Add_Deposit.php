fd137e83315a249a04cff662982232014ea476cb
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-17 20:00:57 +0200

    add files

--- controller/Controller_Add_Deposit.php
+++ controller/Controller_Add_Deposit.php
@@ -23,7 +23,7 @@ else if(empty($categ)){
 }
 else
 {
-  $userid=Users::Get_Userid_By_Nick($nick);
+  $userid=Users::Get_Userid_By_Nick($nick)['userid'];
   $citycheck=City::Get_City_By_Name($city);
 
   if(empty($userid)){
