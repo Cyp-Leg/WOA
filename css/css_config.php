b351f1c85044ab85dc042121c37ca1bec957c9e3
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-17 11:57:08 +0200

    add files

--- css/css_config.php
+++ css/css_config.php
@@ -1,7 +1,7 @@
 
 <html>
 	<link rel="stylesheet" href="css/materialize/css/materialize.min.css">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/style.css">
 	<script src="./materialize/js/materialize.min.js"></script>
 	<script src="./jQuery.js"></script>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
