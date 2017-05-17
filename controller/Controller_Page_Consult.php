1b9db9020cd2650d536e3dcc554ea5b4c3c08584
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-17 13:26:46 +0200

    add files

--- controller/Controller_Page_Consult.php
+++ controller/Controller_Page_Consult.php
@@ -1,6 +1,7 @@
 <?php
 require_once("model/Announce.php");
-require_once("model/City.php")
+require_once("model/City.php");
+
 $lesAnnonces = Announce::Get_All_Announces();
 
 require_once("view/consult.php");
