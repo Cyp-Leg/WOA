6a9b20abaac72832f875ec6fd8bed055f72e1362
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-18 21:10:51 +0200

    css

---
+++ controller/Controller_Etat_Utilisateur.php
@@ -0,0 +1,26 @@
+<?php
+require_once("model/Users.php");
+
+function isLogged(){
+  if(isset($_COOKIE['cookieperso'])){
+    $cookie=$_COOKIE['cookieperso'];
+    $user=Users::Get_User_Id($cookie);
+
+    return(!empty($user));
+  }
+  else return false;
+}
+
+function loggedOnly(){
+  if(!isLogged()){
+    header("Location: ../Connexion.php");
+  }
+}
+
+function unloggedOnly(){
+  if(isLogged()){
+    header("Location: ../Accueil.php");
+  }
+}
+
+} ?>
--- controller/Controller_Page_Connexion.php
+++ controller/Controller_Page_Connexion.php
@@ -1,3 +1,4 @@
 <?php
+require_once('controller/Controller_Etat_Utilisateur.php');
 require_once('view/connexion.php');
 ?>
