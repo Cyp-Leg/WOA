43ad9e0fc2be8ca0a311f30df87f963e14a85b5c
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-18 11:01:56 +0200

    test

--- controller/Controller_Add_Deposit.php
+++ controller/Controller_Add_Deposit.php
@@ -27,7 +27,7 @@ else
   $citycheck=City::Get_City_By_Name($city);
 
   if(empty($userid)){
-    header('Location: ../Deposit4.php');
+    header('Location: ../Deposit.php?userid='.$userid.'');
     ?>
     <script type="text/javascript">alert('Pseudo inexistant');</script>
     <?php
