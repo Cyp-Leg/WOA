f86168d14a6c1a04fddc2b1a1971fd0723342331
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-18 22:02:58 +0200

    css

--- view/connexion.php
+++ view/connexion.php
@@ -18,7 +18,7 @@
 		<?php require ("view/header.php");?>
 		<div>
 			<h4 class="center-align">Connexion</h4>
-      <div class="general-container">
+      <div class="container">
         <form class="col s12" method="POST" action="controller/Controller_Connexion.php">
           <div class="row">
             <div class="input-field col s12">
