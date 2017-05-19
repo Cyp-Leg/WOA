6321e6428487cccdd51b05b59b1e938f29b14b30
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-19 14:02:19 +0200

    test

--- css/css_config.php
+++ css/css_config.php
@@ -2,8 +2,8 @@
 <html>
 	<link rel="stylesheet" href="css/materialize/css/materialize.min.css">
 	<link rel="stylesheet" href="css/style.css">
-	<script src="./materialize/js/materialize.min.js"></script>
-	<script src="./jQuery.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="../css/materialize/js/materialize.min.js"></script>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="icon" type="image/png" href="medias/favicon.ico" />
 
