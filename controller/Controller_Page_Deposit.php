1163e0e9dc13c78147c9ee528f7c7ae387b1282c
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-19 09:18:59 +0200

    login

--- controller/Controller_Page_Deposit.php
+++ controller/Controller_Page_Deposit.php
@@ -1,6 +1,6 @@
 <?php
 require_once("model/Category.php");
-require_once("../controller/Controller_Etat_Utilisateur.php");
+require_once("controller/Controller_Etat_Utilisateur.php");
 
 $lesCategories=Category::Get_All_Categories();
 
