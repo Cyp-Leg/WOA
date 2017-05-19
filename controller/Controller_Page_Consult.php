a97a19335711528903874ca2cb0ba898888d4f35
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-19 09:17:49 +0200

    login

--- controller/Controller_Page_Consult.php
+++ controller/Controller_Page_Consult.php
@@ -1,6 +1,7 @@
 <?php
 require_once("model/Announce.php");
 require_once("model/City.php");
+require_once("../controller/Controller_Etat_Utilisateur.php");
 
 $lesAnnonces = Announce::Get_All_Announces();
 
--- controller/Controller_Page_Inscription.php
+++ controller/Controller_Page_Inscription.php
@@ -1,5 +1,7 @@
 <?php
 require_once("model/Users.php");
+require_once("../controller/Controller_Etat_Utilisateur.php");
+unloggedOnly();
 
 $lesUsers=Users::Get_All_Users();
 
