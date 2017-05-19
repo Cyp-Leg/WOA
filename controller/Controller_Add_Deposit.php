23d479c6f8a5d1b98a1495131e48573b6da955bb
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-19 13:58:40 +0200

    test

--- controller/Controller_Add_Deposit.php
+++ controller/Controller_Add_Deposit.php
@@ -39,6 +39,7 @@ else
     {
       City::Add_City($city);
       $city=City::Get_City_By_Name($city);
+      header("Location: ../Erreur?erreur=".$city);
     }
     Announce::Add_Announce($title,$descrip,$price,$city,$categ,$userid);
     header('Location: ../Consult.php');
