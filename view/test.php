db82fc4c8c2076434ea1b13dd56aa9b0f6395163
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-20 16:43:43 +0200

    add photos

--- view/test.php
+++ view/test.php
@@ -1,2 +1,23 @@
 <?php
-var_dump($_FILES); ?>
+var_dump($_FILES);
+
+$dir_info='../medias/annpics';
+$dir=opendir($dir_info) or die('Erreur : le dossier n\'existe pas.');
+$fichier=array();
+
+while($element = readdir($dir)) {
+	if($element != '.' && $element != '..') {
+	   $fichier[]=$element;
+  }
+}
+
+closedir($dir);
+
+echo "Liste des fichiers/documents accessibles dans '$dir_nom' : \n\n";
+	echo "\t\t<ul>\n";
+		foreach($fichier as $lien) {
+			echo "\t\t\t<li><a href=\"$dir_nom/$lien \">$lien</a></li>\n";
+		}
+	echo "\t\t</ul>";
+
+?>
