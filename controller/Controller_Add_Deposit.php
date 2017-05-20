eb78f7cf3fb1e85656f77d0f323774d834403309
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-20 13:52:04 +0200

    test announce

--- controller/Controller_Add_Deposit.php
+++ controller/Controller_Add_Deposit.php
@@ -23,7 +23,7 @@ else if(empty($categ)){
 }
 else
 {
-  $descrip = pg_escape_string($descip);
+  $descrip = pg_escape_string($descrip);
   $city=strtoupper($city);
   $userid=Users::Get_User_Id($cookie);
   $citycheck=City::Get_City_By_Name($city);
