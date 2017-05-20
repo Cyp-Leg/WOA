bdc0aee783d0432f4db9404aaae9ef846bebb695
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-20 15:55:27 +0200

    add photos

--- view/deposit.php
+++ view/deposit.php
@@ -64,8 +64,6 @@
 											</div>
 										</div>--><label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
 
-     												 <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-
      												 <input type="file" name="mon_fichier" id="mon_fichier" /><br /></td>
 									</tr>
 
