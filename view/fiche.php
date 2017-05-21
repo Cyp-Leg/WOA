0536d80be722cb0aa50c1cbd1b542ecfd6488e21
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-21 16:00:10 +0200

    css

--- view/fiche.php
+++ view/fiche.php
@@ -19,23 +19,20 @@
 			<h4 class="center-align"><?php echo $annprop['announcetitle']; ?></h4>
 		</div>
 
-    <div class="container">
-      <div>
-        <div class="annpic">
+    <div class="container">ss="annpic">
+      <?php
+        if(empty($photoname)){$photoname="errpic.png";}
+        echo "<img src=\"medias/annpics/".$photoname."\" class=\"left-photo\"/>";
+      ?>
+      </div>
+      <div class="ann-caract">
+        <ul>
           <?php
-          if(empty($photoname)){$photoname="errpic.png";}
-          echo "<img src=\"medias/annpics/".$photoname."\" class=\"left-photo\"/>";
+            echo "<li>Catégorie : ".$cat."</li>";
+            echo "<li>Prix : ".$annprop['announceprice']." € </li>";
+            echo "<li>Ville : ".$city."</li>";
           ?>
-        </div>
-        <div class="ann-caract">
-          <ul>
-            <?php
-              echo "<li>Catégorie : ".$cat."</li>";
-              echo "<li>Prix : ".$annprop['announceprice']." € </li>";
-              echo "<li>Ville : ".$city."</li>";
-            ?>
-          </ul>
-        </div>
+        </ul>
       </div>
       <div class="ann-descrip">
         <?php echo $annprop['announcedescrip']; ?>
