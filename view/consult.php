2705745dc0accc930eac59ca2b746f3cb2c52816
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-21 16:16:17 +0200

    css

--- view/consult.php
+++ view/consult.php
@@ -36,7 +36,8 @@
 	          foreach($lesAnnonces as $row)
 	            {
 								$photoname = Photo::Get_Photo_By_Annid($row['announceid']);
-								if( empty($photoname) || !(file_exists("medias/annpics/{$photoname}")) )
+								$exist = file_exists("medias/annpics/{$photoname}");
+								if( empty($photoname) || !($exist) )
 								{
 									$photoname="errpic.png";
 								}
