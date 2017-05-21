5f254898c2304981b549a4a59afa33668ede7015
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-21 23:06:31 +0200

    test

--- controller/Controller_Page_Order.php
+++ controller/Controller_Page_Order.php
@@ -1,6 +1,7 @@
 <?php
   require_once("model/Announce.php");
   require_once("model/Users.php");
+  require_once("controller/Controller_Etat_Utilisateur.php");
   $annid=htmlspecialchars($_GET['prod']);
   $userid=Announce::Get_Announce_User($annid);
   $usersmail=Users::Get_Users_Mail($userid);
