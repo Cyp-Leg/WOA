c1177b2978acc6d0e266556b3b448c3c5b2b03ee
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-22 12:26:56 +0200

    test

--- controller/Controller_Validate.php
+++ controller/Controller_Validate.php
@@ -1,6 +1,6 @@
 <?php
   //require_once('model/Orders.php');
-  require_once('controller/Controller_Etat_Utilisateur');
+  require_once('controller/Controller_Etat_Utilisateur.php');
 
   $userid=htmlspecialchars($_POST['userid']);
   $announceid=htmlspecialchars($_POST['announceid']);
