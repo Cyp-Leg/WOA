6ed1045f4d0e2c33b7240243f608f996568ac4e2
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-22 12:32:49 +0200

    test

--- controller/Controller_Validate.php
+++ controller/Controller_Validate.php
@@ -4,7 +4,7 @@
 
   $userid=htmlspecialchars($_POST['userid']);
   $announceid=htmlspecialchars($_POST['announceid']);
-  $quantity=htmlspecialchars($_POST['quantity']);
+  $quantity=htmlspecialchars($_POST['prodQuantity']);
   $date=date('Y\-m\-d');
 
   require_once('view/validate.php');
