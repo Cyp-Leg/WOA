5f00c6511f93fdb9958e13a46e89f4030dcfd08a
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-23 00:36:13 +0200

    test admin

--- controller/Controller_Page_Admin.php
+++ controller/Controller_Page_Admin.php
@@ -2,6 +2,8 @@
 require_once('controller/Controller_Etat_Utilisateur.php');
 adminOnly();
 require_once('model/Announce.php');
+require_once('model/Photo.php');
+require_once('model/City.php');
 
 $lesAnnonces=Announce::Get_All_Announces();
 
