6fb76c327c9a9c1a3106a67d33d28fa5366eb7d1
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-23 11:39:32 +0200

    deletion

--- controller/Controller_Suppression_Announce.php
+++ controller/Controller_Suppression_Announce.php
@@ -2,7 +2,7 @@
 require_once("../model/Announce.php");
 require_once("../model/Photo.php");
 //require_once("../controller/Controller_Etat_Utilisateur.php");
-$annid=htmlspecialchars($_GET['annid']);
+$annid=htmlspecialchars($_GET['announceid']);
 
 Photo::Delete_Photo_From_Announce($annid);
 Announce::Delete_Announce($annid);
