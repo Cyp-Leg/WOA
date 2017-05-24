160d6be67dfa01a1b6aecf5493b6fadd43de0f15
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-24 19:45:46 +0200

    test js

--- css/hider.js
+++ css/hider.js
@@ -1,24 +1,3 @@
-/*var affichage = document.getElementById('connexionBoutton');
-
-affichage.onclick = function(){
-  if(document.getElementById('connexion').style.display == "none"){
-    document.getElementById('connexion').style.display = "block";
-    document.getElementById('inscription').style.display = "none";
-  }
-  else{
-    document.getElementById('connexion').style.display = "none";
-  }
-}*/
-
-
-/*d'ailleurs tu peux aussi faire function afficherConnection(){
- document.getElementByID("newNom") etc
-}
- et dans ce cas là bon bouton doit avoir ça id="modifierNom" onclick="afficherNom();"
-
-Sinon dans le cas que je t'avais envoyé avant tu mets juste l'id dans le bouton : id="inscriptionBoutton"*/
-
-
 function afficherEvenement(){
   if(document.getElementById("event").style.display == "none")
   {
