7b005b89f07e043f6d0e062f15b21711267bb9ca
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-24 21:26:16 +0200

    add product and events

--- controller/Controller_Page_Consult.php
+++ controller/Controller_Page_Consult.php
@@ -3,6 +3,8 @@ require_once("model/Announce.php");
 require_once("model/City.php");
 require_once("controller/Controller_Etat_Utilisateur.php");
 require_once("model/Photo.php");
+require_once("model/Product.php");
+require_once("model/Events.php");
 
 $lesAnnonces = Announce::Get_All_Announces();
 $lesProduits = Product::Get_All_Products();
