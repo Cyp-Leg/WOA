a7564a16b67d58359870ab3f58598d6810371f4f
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-25 01:32:30 +0200

    last changes

--- controller/Controller_Page_Consult.php
+++ controller/Controller_Page_Consult.php
@@ -7,7 +7,7 @@ require_once("model/Product.php");
 require_once("model/Events.php");
 
 $lesAnnonces = Announce::Get_All_Announces();
-$lesProduits = Announce::Get_All_Announces_From_Products();
+$lesProduits = Announce::Get_All_Announces_From_Product();
 $lesEvents = Announce::Get_All_Announces_From_Events();
 
 require_once("view/consult.php");
