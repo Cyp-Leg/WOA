947c6ae3668d45d03d96e7fd7e60d6e6021e4351
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-25 15:33:37 +0200

    test order

--- controller/Controller_Suppression_User.php
+++ controller/Controller_Suppression_User.php
@@ -3,6 +3,8 @@ require_once("../model/Announce.php");
 require_once("../model/Photo.php");
 require_once("../model/Orders.php");
 require_once("../model/Users.php");
+require_once("../model/Product.php");
+require_once("../model/Events.php");
 //require_once("../controller/Controller_Etat_Utilisateur.php");
 $userid=htmlspecialchars($_GET['userid']);
 
