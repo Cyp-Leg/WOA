7d45cd46d4e89e268f4b681c6e1c5c19b3f21a4f
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-25 12:55:47 +0200

    add js

--- view/consult.php
+++ view/consult.php
@@ -50,7 +50,6 @@
 								if(empty($photoname)){$photoname="medias/avatars/errpic.png";}
 
 								echo "<tr><td><img src=\"".$photoname."\" class=\"annpic\" alt=\"Photographie du produit\"/></td>";
-								echo "<td>".$photoname."</td>";
 								echo "<td><a href=\"Fiche.php?ann=".$row['announceid']."\">".$row['announcetitle']."</a></td>";
 								$cityname = City::Get_City_Name($row['cityid']);
 								echo "<td>".$cityname."</td>";
@@ -79,6 +78,7 @@
 									$photoname = Photo::Get_Photo_By_Annid($row['announceid']);
 									if(empty($photoname)){$photoname="errpic.png";}
 									echo "<tr><td><img src=\"medias/avatars/".$photoname."\" class=\"annpic\" /></td>";
+									echo "<td>".$photoname."</td>";
 		              echo "<td><a href=\"Fiche.php?ann=".$row['announceid']."\">".$row['announcetitle']."</a></td>";
 		              $cityname = City::Get_City_Name($row['cityid']);
 		              echo "<td>".$cityname."</td>";
