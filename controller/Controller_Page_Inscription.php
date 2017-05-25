299f1a90805d1f0a0c6c9ba750c9eff2f1dd12e3
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-25 14:07:05 +0200

    test order

--- controller/Controller_Page_Inscription.php
+++ controller/Controller_Page_Inscription.php
@@ -1,6 +1,6 @@
 <?php
 require_once("model/Users.php");
-require_once("../controller/Controller_Etat_Utilisateur.php");
+require_once("controller/Controller_Etat_Utilisateur.php");
 unloggedOnly();
 
 $lesUsers=Users::Get_All_Users();
