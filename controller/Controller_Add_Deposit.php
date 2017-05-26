e15d45e634632bc4bf353274fb79e2272a966514
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-26 12:36:09 +0200

    date check

--- controller/Controller_Add_Deposit.php
+++ controller/Controller_Add_Deposit.php
@@ -59,6 +59,7 @@ else
       $message="Date invalide, merci de respecter le format JJ-MM-AAAA!";
       header("Location: ../Erreur.php?erreur=".$message);
     }
+    else $eventdate=$datecheck['year']."-".$datecheck['month']."-".$datecheck['day'];
   }
   $descrip = pg_escape_string($descrip);
   $price = str_replace(",",".",$price);
