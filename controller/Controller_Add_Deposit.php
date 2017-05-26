979804905a4dd908ce043709534ba97f8acf18cb
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-26 12:42:12 +0200

    date check

--- controller/Controller_Add_Deposit.php
+++ controller/Controller_Add_Deposit.php
@@ -52,48 +52,49 @@ else if(!is_numeric($price))
 }
 else
 {
-  if(isset($_POST['eventdate']))
+  if(!empty($eventdate))
   {
     $datecheck=date_parse_from_format("Y\-m\-d", $eventdate);
     if (!checkdate($datecheck['month'], $datecheck['day'], $datecheck['year'])) {
-      $message="Date invalide, merci de respecter le format JJ-MM-AAAA!";
+      $message="Date invalide, merci de respecter le format AAAA-MM-JJ!";
       header("Location: ../Erreur.php?erreur=".$message);
     }
-    else $eventdate=$datecheck['year']."-".$datecheck['month']."-".$datecheck['day'];
-    header("Location: ../Erreur.php?erreur=".$eventdate);
-  }
-  $descrip = pg_escape_string($descrip);
-  $price = str_replace(",",".",$price);
-  $city=strtoupper($city);
-  $userid=Users::Get_User_Id($cookie);
-  $citycheck=City::Get_City_By_Name($city);
-
-  if(empty($userid)){
-    $message = "Vous devez être connecté pour poster une annonce!".$userid;
-    header("Location: ../Erreur.php?erreur=".$message);
   }
   else
   {
-    if(empty($citycheck))
-    {
-      City::Add_City($city);
-    }
-    $city=City::Get_City_By_Name($city);
-    $city=$city['cityid'];
-    Announce::Add_Announce($title,$descrip,$price,$city,$categ,$userid);
-    $annid=Announce::Get_Last_Announce_Id();
-    Photo::Add_Photo($announcePic,$annid);
+    $descrip = pg_escape_string($descrip);
+    $price = str_replace(",",".",$price);
+    $city=strtoupper($city);
+    $userid=Users::Get_User_Id($cookie);
+    $citycheck=City::Get_City_By_Name($city);
 
-    if($anntype=="event")
-    {
-      Events::Add_Event($eventdate,$eventplaces,$annid);
+    if(empty($userid)){
+      $message = "Vous devez être connecté pour poster une annonce!".$userid;
+      header("Location: ../Erreur.php?erreur=".$message);
     }
-    else if($anntype=="product")
+    else
     {
-      Product::Add_Product($quantity,$annid);
-    }
+      if(empty($citycheck))
+      {
+        City::Add_City($city);
+      }
+      $city=City::Get_City_By_Name($city);
+      $city=$city['cityid'];
+      Announce::Add_Announce($title,$descrip,$price,$city,$categ,$userid);
+      $annid=Announce::Get_Last_Announce_Id();
+      Photo::Add_Photo($announcePic,$annid);
 
-    header('Location: ../Consult.php');
+      if($anntype=="event")
+      {
+        Events::Add_Event($eventdate,$eventplaces,$annid);
+      }
+      else if($anntype=="product")
+      {
+        Product::Add_Product($quantity,$annid);
+      }
+
+      header('Location: ../Consult.php');
+    }
   }
 }
 
