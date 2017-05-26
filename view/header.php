03715bb47015c3ee58dbe3f4f7848e7834f7a2df
cyp-leg <cyprien_legrand@hotmail.fr>
2017-05-26 11:53:12 +0200

    logo

--- view/header.php
+++ view/header.php
@@ -1,7 +1,7 @@
 <nav>
 	<div class="nav-wrapper greyperso">
-		<a href="/Accueil.php" class="logosite"> LCDE</a>
 		<a href="" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+		<a href="/Accueil.php" class="logosite"> LCDE</a>
 		<ul id="nav-mobile" class="right hide-on-med-and-down">
 			<li><a href="Accueil.php">Accueil</a></li>
 			<li><a href="Deposit.php">Désposer une annonce</a></li>
