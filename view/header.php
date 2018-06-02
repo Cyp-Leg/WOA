02c7e054f836b373486d7b6c5646681458263f7b
cyp-PC\cyp <cyprien_legrand@hotmail.fr>
2018-06-03 01:11:49 +0200

    add .htaccess

--- view/header.php
+++ view/header.php
@@ -3,22 +3,22 @@
 		<a href="" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 		<a href="/Accueil.php" class="logosite"> LCDE</a>
 		<ul id="nav-mobile" class="right hide-on-med-and-down">
-			<li><a href="/Accueil.php">Accueil</a></li>
+			<li><a href="/accueil">Accueil</a></li>
 			<li><a href="/deposit">Désposer une annonce</a></li>
-			<li><a href="/Consult.php">Consulter les annonces</a></li>
-			<?php if(isLogged()){ ?><li><a href="Personnal_orders.php">Mes commandes</a></li><?php } ?>
-			<?php if(isAdmin()){ ?><li><a href="Administrateur.php">Administrateur</a></li><?php } ?>
-		  <?php if(!isLogged()){ ?><li><a href="Connexion.php">Connexion</a></li><?php } ?>
-			<?php if(isLogged()){ ?><li><a title="Déconnexion" href="controller/Controller_Deconnexion.php"><i class="large material-icons">power_settings_new</i></a></li><?php } ?>
+			<li><a href="/annonces">Consulter les annonces</a></li>
+			<?php if(isLogged()){ ?><li><a href="/mescommandes">Mes commandes</a></li><?php } ?>
+			<?php if(isAdmin()){ ?><li><a href="/administrateur">Administrateur</a></li><?php } ?>
+		  <?php if(!isLogged()){ ?><li><a href="/connexion">Connexion</a></li><?php } ?>
+			<?php if(isLogged()){ ?><li><a title="Déconnexion" href="/controller/Controller_Deconnexion.php"><i class="large material-icons">power_settings_new</i></a></li><?php } ?>
 		</ul>
 		<ul class="side-nav left-aligned" id="mobile-demo">
-			<li><a class="waves-effect waves-light btn" href="/Accueil.php"><i class="large material-icons">store</i>Accueil</a></li>
+			<li><a class="waves-effect waves-light btn" href="/accueil"><i class="large material-icons">store</i>Accueil</a></li>
 			<li><a class="waves-effect waves-light btn" href="/deposit"><i class="large material-icons">play_circle_outline</i>Désposer une annonce</a></li>
-			<li><a class="waves-effect waves-light btn" href="/Consult.php"><i class="large material-icons">replay</i>Voir les annonces</a></li>
-			<?php if(isLogged()){ ?><li><a class="waves-effect waves-light btn" href="Personnal_orders.php"><i class="large material-icons">class</i>Mes commandes</a></li><?php } ?>
-			<?php if(isAdmin()){ ?><li><a class="waves-effect waves-light btn" href="Administrateur.php"><i class="large material-icons">supervisor_account</i>Administrateur</a></li><?php } ?>
-			<?php if(!isLogged()){ ?><li><a class="waves-effect waves-light btn" href="Connexion.php"><i class="large material-icons">input</i>Connexion</a></li><?php } ?>
-			<?php if(isLogged()){ ?><li><a class="waves-effect waves-light btn" href="controller/Controller_Deconnexion.php"><i class="large material-icons">power_settings_new</i>Déconnexion</a></li><?php } ?>
+			<li><a class="waves-effect waves-light btn" href="/annonces"><i class="large material-icons">replay</i>Voir les annonces</a></li>
+			<?php if(isLogged()){ ?><li><a class="waves-effect waves-light btn" href="mescommandes"><i class="large material-icons">class</i>Mes commandes</a></li><?php } ?>
+			<?php if(isAdmin()){ ?><li><a class="waves-effect waves-light btn" href="administrateur"><i class="large material-icons">supervisor_account</i>Administrateur</a></li><?php } ?>
+			<?php if(!isLogged()){ ?><li><a class="waves-effect waves-light btn" href="connexion"><i class="large material-icons">input</i>Connexion</a></li><?php } ?>
+			<?php if(isLogged()){ ?><li><a class="waves-effect waves-light btn" href="/controller/Controller_Deconnexion.php"><i class="large material-icons">power_settings_new</i>Déconnexion</a></li><?php } ?>
 		</ul>
 	</div>
 </nav>
