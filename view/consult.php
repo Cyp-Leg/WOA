916dffac28be9562a3e7531ed852df5ba64835d3
cyp-PC\cyp <cyprien_legrand@hotmail.fr>
2018-06-03 00:40:23 +0200

    add .htaccess

--- view/consult.php
+++ view/consult.php
@@ -50,7 +50,7 @@
 								if(empty($photoname)){$photoname="medias/avatars/errpic.png";}
 
 								echo "<tr><td><img src=\"".$photoname."\" class=\"annpic\" alt=\"Photographie du produit\"/></td>";
-								echo "<td><a href=\"Fiche.php?ann=".$row['announceid']."\">".$row['announcetitle']."</a></td>";
+								echo "<td><a href=\"annonce/".$row['announceid']."\">".$row['announcetitle']."</a></td>";
 								$cityname = City::Get_City_Name($row['cityid']);
 								echo "<td>".$cityname."</td>";
 								echo "<td>".$row['announceprice']."</td></tr>";
