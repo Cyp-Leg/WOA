29a7173f50ce38cbd1843cd8cd3499df20dbd31a
cyp-PC\cyp <cyprien_legrand@hotmail.fr>
2018-06-03 01:17:22 +0200

    add .htaccess

--- view/fiche.php
+++ view/fiche.php
@@ -11,7 +11,7 @@
 		<meta name="Author" content="CyprienLegrand">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-		 <?php include ("../css_config.php"); ?>
+		 <?php include ("../../css_config.php"); ?>
 	</head>
 	<body>
 		<?php require ("view/header.php");?>
