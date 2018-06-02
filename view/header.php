2788f8f646d77c34eaacec1e042aa2a7b99ad8dc
cyp-PC\cyp <cyprien_legrand@hotmail.fr>
2018-06-03 01:26:09 +0200

    add .htaccess

--- view/header.php
+++ view/header.php
@@ -1,7 +1,7 @@
 <nav>
 	<div class="nav-wrapper greyperso">
 		<a href="" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-		<a href="/Accueil.php" class="logosite"> LCDE</a>
+		<a href="/accueil" class="logosite"> LCDE</a>
 		<ul id="nav-mobile" class="right hide-on-med-and-down">
 			<li><a href="/accueil">Accueil</a></li>
 			<li><a href="/deposit">Désposer une annonce</a></li>
